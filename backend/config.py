966d817f37602307feb91558b0b51e33f6700bc8
alvinpl <alvinpl21@gmail.com>
2024-12-27 15:06:53 -0500

    started setting up config file

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import sqlalchemy
+from flask_cors import CORS, cross
+
+app = Flask(__name__)
+CORS(app)
+
+# Point to a local SQLite database file (stored in backend/).
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
